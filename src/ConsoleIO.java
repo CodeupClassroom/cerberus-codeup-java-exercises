6b387bfe4d4e6d68962a15aecfb900be5fcdf214
Lola Castiilo <larrycastillo2009@gmail.com>
2023-05-15 12:18:58 -0500

    update

---
+++ src/ConsoleIO.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class ConsoleIO {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int userInput;
+
+        System.out.println("Enter something: ");
+        userInput= sc.nextInt();
+
+        System.out.println("You entered:" + userInput);
+
+//        println example
+//        System.out.println("Hello world! (๑ᵔ⤙ᵔ๑) ");
+//        System.out.println("(๑ᵔ⤙ᵔ๑) ");
+
+
+//        print example
+//        System.out.print("Hello ");
+//        System.out.print("World");
+//        System.out.print("! (๑ᵔ⤙ᵔ๑) ");
+
+
+
+
+
+//        printf(print formatted) example #1
+//        String name = "Lola";
+//        System.out.printf("Hello there, %s. Nice to see you.%n", name);
+//        System.out.println("hi");
+
+
+
+
+
+
+//        printf example #2
+//        String greeting = "Howdy";
+//        String cohortName = "Cerberus";
+//        System.out.printf("%s, %s!", greeting, cohortName);
+
+
+//        printf example #3
+//        long n = 461012;
+//        System.out.printf("%d%n",n);
+//        System.out.printf("%08d%n",n);
+//        System.out.printf("%,8d%n",n);
+
+
+//        printf example #4
+//        double pi = Math.PI;
+//        System.out.printf("%f%n",pi);
+//        System.out.printf("%.3f%n",pi);
+//        System.out.printf("%10.3f%n",pi);
+
+
+    }
+
+
+
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,51 @@
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello,World!");
+//        System.out.println("Hello,World!");
+//        1
+//        int myFavoriteNumber = 3;
+//        String myString = 'a';
+//        System.out.println(myString);
+
+//        float myNumber;
+//        myNumber = 3.14F ;
+//        System.out.println(myNumber);
+
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//
+//        System.out.println(y);
+
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+//
+//        System.out.println(y);
+
+ byte smolNum = 127;
+        System.out.println(++smolNum);
+
+
+
+
+
+
+
+
+
+
+
     }
 }
