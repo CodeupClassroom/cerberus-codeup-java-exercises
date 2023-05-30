dcb26ce9bcc1a197ff2a62521d96713475e2a944
bob <bob@bob.com>
2023-05-29 21:12:46 -0500

    pre coll lec

---
+++ src/docrob/CollectionsLec.java
@@ -0,0 +1,136 @@
+package docrob;
+
+import java.util.*;
+
+public class CollectionsLec {
+    public static void main(String[] args) {
+
+        // explain Collections
+        // Lists, Sets, Maps
+
+        // ************* ARRAYLIST
+
+        // explain ArrayList
+
+        // declare an ArrayList
+        // explain <String>
+        // add a few elements
+        // try to add something not String
+
+        // can also give it any # of string args
+
+        // output an arraylist
+
+        // use ArrayList initializer
+        // explain Arrays.asList function
+
+
+        // show size() instead of .length
+        // add another element
+        // print size()
+
+
+        // get an element at a given index
+
+
+        // show loops with ArrayList
+
+        // for i
+
+        // foreach
+
+        // what happens if you change the food variable's value? does it change the arraylist?
+
+
+        // show .indexOf()
+        // is indexOf() overloaded ?
+        // show .lastIndexOf()
+        // is there a .contains() ?
+
+
+        // show .remove()
+//        foods.remove("cake");
+//        foods.removeAll(Arrays.asList("apple", "cake"));
+
+        // show .removeAll (will need Arrays.asList() )
+
+
+        // show isEmpty()
+
+
+        // SHOW AN EXAMPLE OF A CLASS WITH AN ARRAYLIST INSIDE IT
+        // create an Arena class
+        // start with an array of fighters.
+        // remind them about encapsulation
+        // what happens if we directly use the Arena's public array
+        // and then later change our mind to use an ArrayList?
+        // now use accessor for returning an array of items
+
+        // what supporting methods are handy for working with the Arena's fighters?
+
+
+        // ************* HASHMAP
+
+        // explain hashmap
+        // terms: key value
+        // use diagram of file cabinet
+
+        // show hashmap
+        // use a roster of Fighters for the hashmap
+
+        // show .put()
+
+        // print hashmap
+
+        // does it overwrite or duplicate if we put again?
+
+        // show putIfAbsent
+
+        // show .get()
+
+        // show get if key is absent
+
+        // show .getOrDefault()
+
+        // show .containsKey
+
+        // show .containsValue
+        // may need to define .equals() for this
+
+        // show .replace with 2 and 3 args
+        // print hashmap
+
+        // show .remove()
+
+        // what if we try to remove a key that is not there?
+
+        // print hashmap
+
+        // show .isEmpty()
+
+        // show .clear()
+        // print hashmap
+
+        // show how to iterate over a hashmap
+        // use .keySet()
+        // and foreach over the set with a .get()
+
+
+        // ************* SET
+
+        // make a HashSet
+
+        // add a few elements
+
+        // print the set
+
+        // add a duplicate
+
+        // print the set
+
+        // iterate over the set
+
+        // look thru other methods
+
+    }
+}
