48a1d9a5f6cc464eae8b27ebd35d1e199a006384
bob <bob@bob.com>
2023-05-31 21:15:30 -0500

    pre file lec

---
+++ src/docrob/FileLec.java
@@ -0,0 +1,104 @@
+package docrob;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FileLec {
+    public static void main(String[] args) {
+
+        // explain relative vs. absolute paths
+        // use the terminal below to show this using cat or ls
+
+        // Path objects
+        // use Paths.get to get a path ref to dir and a file
+        // relative
+        // then absolute
+
+        Path aDir = Paths.get("adir");
+        Path aFile = Paths.get("adir", "afile.txt");
+
+        System.out.println(Files.exists(aDir));
+        if(!Files.exists(aDir)) {
+            try {
+                Files.createDirectory(aDir);
+                System.out.println("dir created");
+            } catch (IOException e) {
+                System.out.println("createDirectory exception: " + e.getMessage());
+            }
+        } else {
+            System.out.println("the directory exists already");
+        }
+
+        // an alternative to using an if/else to first check if file exists
+        // try to create it and ignore an already exists exception
+        try {
+            Files.createFile(aFile);
+            System.out.println("the file has been created");
+        } catch(FileAlreadyExistsException e) {
+            System.out.println("the file exists!");
+        } catch (IOException e) {
+            System.out.println("createFile exception: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        // do we have to specify all the dirs as separate args???
+
+        // Files class
+        // .exists() to see dirs and files that exist and don't exist
+        // point at notExists()
+
+        // now try .createDirectory() and .createFile()
+        // feel the IOException
+
+        // write to a file using Files.write
+        // default for StandardOpenOption is create/overwrite
+        // look at other options
+//        List<String> fruits = Arrays.asList("apple", "pear", "banana");
+
+        Fighter bob = new Fighter("bob");
+//        System.out.println(bob.toFileString());
+        Fighter sue = new Fighter("sue");
+
+        List<String> fighterStrings = Arrays.asList(bob.toFileString(), sue.toFileString());
+
+        // make a method called saveCombatants()
+        // and make a .toFileString() method that it calls for extra nice
+
+        try {
+            // , StandardOpenOption.APPEND
+            Files.write(aFile, fighterStrings);
+        } catch (IOException e) {
+            System.out.println("file write exception: " + e.getMessage());
+        }
+
+        // what are we trying to write to a file?
+        // how should we write an object to a text file???
+
+        // read from a file
+        // Files.readAllLines
+        // Strings are easy... but how do we get other OBJECTS from a file???
+
+        // make a method called loadCombatants
+        // and use a static method called .fromFileString(str)
+        // explain why that is nice
+
+        try {
+            List<String> fileStrings = Files.readAllLines(aFile);
+            Fighter cloneOfBob = Fighter.fromFileString(fileStrings.get(0));
+            System.out.println("cloneOfBob = " + cloneOfBob);
+
+            for (String fileString : fileStrings) {
+                Fighter aFighter = Fighter.fromFileString(fileString);
+            }
+//            System.out.println(fileStrings.size());
+//            System.out.println(fileStrings);
+        } catch (IOException e) {
+            System.out.println("file read exception: " + e.getMessage());
+        }
+
+
+    }
+}
