c3eaf2e04f2e81a37e1b7643246400577a24a855
madmarcos-codeup <mark.robinson@codeup.com>
2023-06-04 12:32:25 -0500

    pre dep lec

---
+++ src/docrob/DependencyLec.java
@@ -0,0 +1,50 @@
+package docrob;
+
+public class DependencyLec {
+    public static void main(String[] args) {
+        // explain jar file
+
+        // show jar file in console adventure game
+
+        // show how it is executable from the command line (is it really?)
+
+        // explain dependency
+
+        // use lombok as an example (go to lombok web site)
+
+        // how to get lombok into our project???
+
+        // introduce Maven
+
+        // create a Maven project
+
+        // show the pom file
+            // explain briefly all parts espec. dependencies and plugins
+
+        // add lombok to the pom file and reload maven
+
+        // demo use of lombok
+
+        // show mvn repository
+
+        // find Google's guava lib and get it into our project
+
+        // use guava
+        /*
+        System.out.println(Splitter.on(',')
+         .trimResults()
+         .omitEmptyStrings()
+         .split("the ,quick, ,brown, fox, jumps, over, the, lazy, little dog."));
+         */
+
+        // point out how you can chain the methods
+            // introduce term fluent interface aka method chaining
+
+        // lastly show how one can EASILY download, build, and run a Maven project
+            // console adventure game
+            // Maven can download and install dependencies, build it, run it, test it, etc. all with a single command
+
+
+
+    }
+}
